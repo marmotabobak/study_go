b84146080fc15f3d60f45505e4aa0eda2cda0fe8
Igor Urvantsev <iurvantsev@inno.tech>
2024-11-09 23:35:43 +0300

    refactoring: separate packages

---
+++ Grade/REST/internal/mux/mux.go
@@ -0,0 +1,31 @@
+package mux
+
+import (
+	"net/http"
+	"REST/internal/rest"
+)
+
+func NewMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			rest.GetTasks(w)
+		case http.MethodPost:
+			rest.AddTask(w, r)
+		default:
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		}
+	})
+	mux.HandleFunc("/tasks/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPut:
+			rest.ChangeTask(w, r)
+		case http.MethodDelete:
+			rest.DeleteTask(w, r)
+		default:
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		}
+	})
+	return mux
+}
